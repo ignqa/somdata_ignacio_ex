99c93ce97e8df85f299736a591576927dff829c6
IgnacioGN <sylvexfuture@gmail.com>
2020-05-28 21:35:40 +0200

    Model created and working fine

--- somdata_ex/models.py
+++ somdata_ex/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class BTCUSDT(models.Model):
+    high = models.FloatField()
+    low = models.FloatField()
+    updated_at = models.DateTimeField()
--- somdata_ex/views.py
+++ somdata_ex/views.py
@@ -1,3 +1,16 @@
+import requests
 from django.shortcuts import render
+from somdata_ex.models import BTCUSDT
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    response = requests.get("https://api.bittrex.com/v3/markets/BTC-USDT/summary")
+    if response.status_code == 200:
+        r_json = response.json()
+        point = BTCUSDT(high=r_json["high"], low=r_json["low"], updated_at=r_json["updatedAt"])
+
+        point.save()
+
+        return HttpResponse(response.json())
